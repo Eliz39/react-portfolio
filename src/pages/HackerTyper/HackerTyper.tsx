edf254a91c1cc2b5c674da9e2cb8a6a20fd3eb93
Eliz39 <liza.nikitina9898@gmail.com>
2022-08-17 07:22:29 +0200

    refactor fetch and useState

--- src/pages/HackerTyper/HackerTyper.tsx
+++ src/pages/HackerTyper/HackerTyper.tsx
@@ -7,30 +7,30 @@ const CHARS_ONKEYDOWN = 7
 
 export const HackerTyper = () => {
   const [codeSource, setCodeSource] = useState('')
-  const [codeOutput, setCodeOutput] = useState(START_MESSAGE)
   const [currentIndex, setCurrentIndex] = useState(0)
   const divWrapperRef = useRef<HTMLDivElement | null>(null)
   const bottomParagraphRef = useRef<HTMLParagraphElement | null>(null)
   useEffect(() => {
+    const getSourceCode = async () => {
+      const res = await fetch('code.txt')
+      setCodeSource(await res.text())
+    }
     divWrapperRef.current?.focus()
-    fetch('code.txt')
-      .then(res => res.text())
-      .then(text => setCodeSource(text))
-  }, [])
+    getSourceCode()
+  })
 
   return (
     <Div_Wrapper
       onKeyDown={e => {
         if (e.key !== 'Escape') {
           setCurrentIndex(currentIndex + CHARS_ONKEYDOWN)
-          setCodeOutput(codeSource.substring(0, currentIndex))
           bottomParagraphRef.current?.scrollIntoView()
-        } else setCodeOutput('')
+        } else setCurrentIndex(0)
       }}
       tabIndex={0}
       ref={divWrapperRef}
     >
-      <Div_Content>{codeOutput}</Div_Content>
+      <Div_Content>{codeSource.substring(0, currentIndex) || START_MESSAGE}</Div_Content>
       <p ref={bottomParagraphRef}></p>
     </Div_Wrapper>
   )
